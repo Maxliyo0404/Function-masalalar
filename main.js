befd237b00fb80e5dc8ccd80a165824fe88f24be
Mahliyo <gasandigital13@gmail.com>
2026-04-06 21:15:08 +0500

    Function masalalar  2-masalani boshladim

--- main.js
+++ main.js
@@ -8,3 +8,9 @@
 // console.log(power(3, 5));
  
 //  2-masala
+function mean(a, b){
+    let orta = Math.random(a, b);
+    let result = Math.pow(a, b);
+    let men = Math.sqrt(orta, result);
+}
+console.log(mean(12, 48));
