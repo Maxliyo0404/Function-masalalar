c3f90c952122cedf455ae45e698cf4eb1cb22087
Mahliyo <gasandigital13@gmail.com>
2026-04-06 21:22:57 +0500

    Function masalalar  2-masalani yakunladim

--- main.js
+++ main.js
@@ -8,10 +8,10 @@
 // console.log(power(3, 5));
  
 //  2-masala
-function mean(a, b){
-    let orta = (a + b) / 2;
-    let son = Math.sqrt(a * b);
-    return[orta, son]
+// function mean(a, b){
+//     let orta = (a + b) / 2;
+//     let son = Math.sqrt(a * b);
+//     return[orta, son]
    
-}
-console.log(mean(12, 48));
+// }
+// console.log(mean(12, 48));
